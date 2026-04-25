0f53ef9892f0a8b9cc69dcd47c83f8a757c64106
kklannd100 <kkland100@gmail.com>
2026-04-25 21:44:19 +0900

    UI 초기작업

---
+++ app/src/main/java/com/example/dropmemo/AddActivity.java
@@ -0,0 +1,22 @@
+package com.example.dropmemo;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.widget.Button;
+import android.view.View;
+
+public class AddActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add);
+
+        Button btnBack = findViewById(R.id.btn_back);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
